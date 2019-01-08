3ba00f564a20a6b9c7d4d98fa86861f7eccc7656
csPython <cspython@AD25TF04NGF1J-40.local>
2019-01-08 11:27:53 -0500

    added code has a response if user entered something wrong and if he got it correct or wrong will give you score.

--- questionsRN.py
+++ questionsRN.py
@@ -1,5 +1,6 @@
 #varibles
 q1C = False
+grade = 0
 
 #intro
 print("you will answer this multiple chocie question")
@@ -12,4 +13,18 @@ print("""what is 2+2?
 3)7
 4)2""")
 
-                 
+#code
+while q1C == False:
+    try:
+        q1 = int(input())
+        if 0 < q1 < 5:
+            q1C = True
+            if q1 == 1:
+                grade+=100
+        else:
+            print("your amswer was not a number 1-4")
+    except ValueError:
+        print("your answer was a letter not a number 1-4")
+
+    print("your score is" , grade , "% out of 100%")
+        
